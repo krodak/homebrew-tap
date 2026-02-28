49deb03184714aad74bb11f6e25d9f23c05fb14e
Krzysztof Rodak <krzysztof@passivelogic.com>
2026-02-28 11:59:35 +0100

    clickup-cli 0.8.0

--- Formula/clickup-cli.rb
+++ Formula/clickup-cli.rb
@@ -1,8 +1,8 @@
 class ClickupCli < Formula
   desc "ClickUp CLI for AI agents and humans"
   homepage "https://github.com/krodak/clickup-cli"
-  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-0.7.0.tgz"
-  sha256 "be238f2eaad97253e97cc50fe9e51546491ebf7c6efb7babad679af819499e3c"
+  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-0.8.0.tgz"
+  sha256 "8983bf1099a97f41ef0e695fa02c7a6ca984d27c6e298ac5ed4a9c6b4e022f9f"
   license "MIT"
 
   depends_on "node"

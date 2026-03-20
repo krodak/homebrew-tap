b298fe349cff7a15da02d564a0b12a04a0b2234e
Krzysztof Rodak <krodak.konta@gmail.com>
2026-03-20 03:27:21 +0100

    clickup-cli 0.21.0 - binary renamed to cup

--- Formula/clickup-cli.rb
+++ Formula/clickup-cli.rb
@@ -1,8 +1,8 @@
 class ClickupCli < Formula
   desc "ClickUp CLI for AI agents and humans"
   homepage "https://github.com/krodak/clickup-cli"
-  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-0.20.0.tgz"
-  sha256 "ed0e91ebc65b18be3ca55fcdd49c9d0c20451bac3e1cc7db785b781ffc62a7e6"
+  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-0.21.0.tgz"
+  sha256 "f806e62477c2b2046dc6183bb536e9a09c69413c8055089a89d519f58d864270"
   license "MIT"
 
   depends_on "node"

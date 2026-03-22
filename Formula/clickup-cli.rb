43ef58c8407036f6e5516e591d42d1170cfda6f3
Krzysztof Rodak <krodak.konta@gmail.com>
2026-03-22 05:58:35 +0100

    clickup-cli 1.5.2 - fix symlink execution

--- Formula/clickup-cli.rb
+++ Formula/clickup-cli.rb
@@ -1,8 +1,8 @@
 class ClickupCli < Formula
   desc "ClickUp CLI for AI agents and humans"
   homepage "https://github.com/krodak/clickup-cli"
-  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-1.5.1.tgz"
-  sha256 "6df80fd90be8ab55f168b56299143d2aa7d84740c3cfdf2a58c14e5225cb5f03"
+  url "https://registry.npmjs.org/@krodak/clickup-cli/-/clickup-cli-1.5.2.tgz"
+  sha256 "4b44aa5a59899ad06bb735d7c2c49ffa016d7b7cb926807d442534473150b516"
   license "MIT"
 
   depends_on "node"
